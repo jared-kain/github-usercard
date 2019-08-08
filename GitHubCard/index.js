74182a94db211b05ac310dba194af9749313b971
jaredkain <jared.r.kain@gmail.com>
2019-08-08 16:07:56 -0500

    WIP: axios GET request complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,18 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+axios.get('https://api.github.com/users/jaredkain')
+  .then( (response) => {
+    console.log(response)
+    response.data.message.forEach( item => {
+      let newUser = userCard(item)
+      entryPoint.appendChild(newUser)
+    })
+    
+  })
+  .catch( (err) => {
+    console.log(err)
+  })
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
